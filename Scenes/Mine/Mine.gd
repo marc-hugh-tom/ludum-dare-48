895ba620216340c2a80a3bb907f69a722a478baf
Hugh Grigg <mhgrigg@fastmail.com>
2021-04-24 22:50:37 +0100

    Blow up mines by shooting them

--- Scenes/Mine/Mine.gd
+++ Scenes/Mine/Mine.gd
@@ -5,17 +5,23 @@ const ExplosionResource = preload("res://Scenes/Explosion/Explosion.tscn")
 
 
 func _on_Mine_body_entered(body):
-	explode(body)
+	cause_damage(body)
+	explode()
 
 
-func explode(victim):
-	print("boom")
-	var explosion = ExplosionResource.instance()
-	explosion.position = position
-	get_parent().add_child(explosion)
-	if victim.has_method("damage"):
+func cause_damage(victim):
+	if victim and victim.has_method("damage"):
 		victim.damage(10)
 	if victim.has_method("impulse"):
 		var direction = ( victim.position - self.position ).normalized()
 		victim.impulse(direction * 3)
+
+func explode():
+	print("boom")
+	var explosion = ExplosionResource.instance()
+	explosion.position = position
+	get_parent().add_child(explosion)
 	destroy()
+
+func take_damage(amount: int = 0):
+	explode()
--- Scenes/Weapons/Harpoon.gd
+++ Scenes/Weapons/Harpoon.gd
@@ -15,3 +15,10 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Area2D_area_entered(area):
+	print("harpoon hit something")
+	if area.has_method("take_damage"):
+		area.take_damage(10)
+	queue_free()
